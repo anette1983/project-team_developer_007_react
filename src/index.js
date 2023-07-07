2ad04f3e5262ad8f38abbabb4b5d1ad88caf1acb
SergSmilya <sergsmilichenko@gmail.com>
2023-07-07 12:02:53 +0300

    Start project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 
+// import fonts
+import './fonts/Helvetica/Helvetica.ttf';
+import './fonts/Inter/Inter-VariableFont_slnt,wght.ttf';
+import './fonts/Montserrat/Montserrat-VariableFont_wght.ttf';
+import './fonts/Poppins/Poppins-Black.ttf';
+import './fonts/Poppins/Poppins-Bold.ttf';
+import './fonts/Poppins/Poppins-Medium.ttf';
+import './fonts/Poppins/Poppins-Regular.ttf';
+import './fonts/Poppins/Poppins-SemiBold.ttf';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/project-team_developer_007_react">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
