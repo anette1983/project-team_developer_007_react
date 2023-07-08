f7909f84cbff7b5d47eef14d1ea6ddea03f32231
SergSmilya <sergsmilichenko@gmail.com>
2023-07-08 17:37:29 +0300

    fixed error code1

--- src/components/Footer/FollowUs/FollowUs.js
+++ src/components/Footer/FollowUs/FollowUs.js
@@ -1,15 +1,22 @@
-import css from "./FollowUs.module.css"
+import css from './FollowUs.module.css';
 
 export const FollowUs = () => {
-    return(
-        <div className={css.container}>
-            <ul className={css.list}>
-                <li><a href=""></a>facebook</li>
-                <li><a href=""></a>youtube</li>
-                <li><a href=""></a>twitter</li>
-                <li><a href=""></a>instagram</li>
-               
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className={css.container}>
+      <ul className={css.list}>
+        <li>
+          <a href="/">/</a>facebook
+        </li>
+        <li>
+          <a href="/">/</a>youtube
+        </li>
+        <li>
+          <a href="/">/</a>twitter
+        </li>
+        <li>
+          <a href="/">/</a>instagram
+        </li>
+      </ul>
+    </div>
+  );
+};
