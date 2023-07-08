855c2e6c2abf78ed9f41fffab1df709fb2eeb546
SergSmilya <110824059+SergSmilya@users.noreply.github.com>
2023-07-08 05:18:01 +0000

    Deploying to gh-pages from @ SergSmilya/project-team_developer_007_react@1032d060539bd48e043e26e3ec9c4f863324de86 🚀

--- static/js/682.9348d8ba.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject_team_developer_007_react=self.webpackChunkproject_team_developer_007_react||[]).push([[682],{4682:function(e,r,n){n.r(r),n.d(r,{default:function(){return i}});var t=n(1087),c=n(184),s=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(t.rU,{to:"/register",children:"Register"}),(0,c.jsx)(t.rU,{to:"/signin",children:"SignIn"})]})},i=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)("h1",{children:"Welcome Page"}),(0,c.jsx)(s,{})]})}}}]);
-//# sourceMappingURL=682.9348d8ba.chunk.js.map
