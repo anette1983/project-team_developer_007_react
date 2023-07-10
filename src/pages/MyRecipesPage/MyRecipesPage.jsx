41ea74f31273c5ee3ed22355e6e8b67e5f24adb8
Hanna <anna_kon@bigmir.net>
2023-07-10 20:51:23 +0300

    added components, some markup, css. In progress

--- src/pages/MyRecipesPage/MyRecipesPage.jsx
+++ src/pages/MyRecipesPage/MyRecipesPage.jsx
@@ -1,5 +1,22 @@
+import { MainPageTitle } from 'components/MainPageTitle/MainPageTitle';
+import css from '../pages.module.css';
+import myRecipesCss from './MyRecipesPage.module.css';
+import MyRecipesList from 'components/MyRecipesList/MyRecipesList';
+
 const MyRecipesPage = () => {
-  return <>MyRecipesPage</>;
+  const text = 'My recipes';
+  return (
+    <>
+      <div className={css.section}>
+        <div className={`${css.container} ${myRecipesCss.container}`}>
+          <MainPageTitle text={text} />
+        </div>
+        <div className={`${css.container} ${myRecipesCss.wrapper}`}>
+          <MyRecipesList />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MyRecipesPage;
