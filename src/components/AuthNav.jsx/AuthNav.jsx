7a75bc5f96b15acfc160dbdfe2ce2e5444d9b83d
Pavel S <neutr00n90@gmail.com>
2023-07-10 08:37:34 +0300

    added basic auth functionality

--- src/components/AuthNav.jsx/AuthNav.jsx
+++ src/components/AuthNav.jsx/AuthNav.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import css from './AuthNav.module.css';
 
 const AuthNav = () => {
-    return (
-      <>
-        <Link to="/register">Register</Link>
-        <Link to="/signin">SignIn</Link>
-      </>
-    );
+  return (
+    <>
+      <div className={css.AuthNav__wrap}>
+        <Link className={css.AuthNav__button} to="/register">
+          Registration
+        </Link>
+        <Link className={css.AuthNav__button} to="/signin">
+          Sign In
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default AuthNav;
