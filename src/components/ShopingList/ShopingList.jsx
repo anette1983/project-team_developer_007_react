5dcca2fbc7c94e69cbdfebaf56fdcfe5abe5831e
SergSmilya <sergsmilichenko@gmail.com>
2023-07-14 08:58:37 +0300

    Update ShopingList.jsx

--- src/components/ShopingList/ShopingList.jsx
+++ src/components/ShopingList/ShopingList.jsx
@@ -1,23 +1,16 @@
-import ShopingItem from 'components/ShopingItem/ShopingItem'
-import css from './ShipongList.module.css'
-import img from '../../picture/image4.png'
+import ShopingItem from 'components/ShopingItem/ShopingItem';
+import css from './ShipongList.module.css';
+// import img from '../../picture/image4.png'
 
 const ShopingList = () => {
-    return (
-        <div className={css.wrap}>
-            <ul className={ css.list}>
-                <ShopingItem
-                    title={'Salmon'}
-                    quantity={5} />
-                 <ShopingItem
-                    title={'Salmon'}
-                    quantity={ 5} />
-            </ul>
-           
-            
+  return (
+    <div className={css.wrap}>
+      <ul className={css.list}>
+        <ShopingItem title={'Salmon'} quantity={5} />
+        <ShopingItem title={'Salmon'} quantity={5} />
+      </ul>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default ShopingList
+export default ShopingList;
