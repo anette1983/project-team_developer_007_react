8d1472e97732553ab54a7b30e75a4d8822798793
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-07-15 11:12:37 +0300

    change baseurl

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://so-yummy-project.onrender.com/';
+axios.defaults.baseURL = 'https://so-yummy-project.onrender.com/';
 
 const authHeader = {
   set(token) {
