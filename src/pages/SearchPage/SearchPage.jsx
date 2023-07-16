c8084d2704da5ce47b3c60edf568097ce262d882
SergSmilya <sergsmilichenko@gmail.com>
2023-07-16 14:22:59 +0300

    changes path pictures by default

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -6,65 +6,61 @@ import searchCss from './searchContainer.module.css';
 import SearchedRecipesList from 'components/SearchedRecipesList/SearchedRecipesList';
 import { Pagination } from '@mui/material';
 import { useEffect, useState } from 'react';
-import {selectRecipes} from "../../redux/recipes/selectors"
+import { selectRecipes } from '../../redux/recipes/selectors';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMoreBySearch, clearRecipes } from 'redux/recipes/operations';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
 import { useSearchParams } from 'react-router-dom';
 
-
 const SearchPage = () => {
   const [page, setPage] = useState(1);
-  const [searchBy, setSearchBy] = useState('search')
-  const [limit, setLimit]=useState(6)
-  const dispatch = useDispatch()
-   const [searchParams, setSearchParams] = useSearchParams();
-  const recipes = useSelector(selectRecipes)
+  const [searchBy, setSearchBy] = useState('search');
+  const [limit, setLimit] = useState(6);
+  const dispatch = useDispatch();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const recipes = useSelector(selectRecipes);
   const isLoged = useSelector(selectIsLoggedIn);
   const query = searchParams.get('query') ?? '';
-  
-    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-      setPage(value);
-     
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
   };
   useEffect(() => {
-    
-    dispatch(clearRecipes())
-  },[dispatch])
-  
+    dispatch(clearRecipes());
+  }, [dispatch]);
+
   useEffect(() => {
-    if (!query) return
-      if (window.innerWidth>=1440) {
-    setLimit(12)
-  }
+    if (!query) return;
+    if (window.innerWidth >= 1440) {
+      setLimit(12);
+    }
     if (isLoged) {
-      dispatch(fetchMoreBySearch({ searchBy, page, limit, query}))
-    }  
-},[dispatch, page, query, limit, searchBy, isLoged])
-  
-  
-   const setParams = (value) => {
-     setSearchParams({ query: value.search, page, limit: 6 })
-    };
-  
+      dispatch(fetchMoreBySearch({ searchBy, page, limit, query }));
+    }
+  }, [dispatch, page, query, limit, searchBy, isLoged]);
+
+  const setParams = value => {
+    setSearchParams({ query: value.search, page, limit: 6 });
+  };
+
   return (
     <div className={css.section}>
       <div className={`${css.container} ${searchCss.container}`}>
         <MainPageTitle text="Search" />
       </div>
       <div className={`${css.container} ${searchCss.container}`}>
-        <SearchForm title={setParams} setSearchBy={setSearchBy } />
+        <SearchForm title={setParams} setSearchBy={setSearchBy} />
       </div>
       {!recipes.length && (
         <div className={`${css.container} ${searchCss.container}`}>
           <img
             className={searchCss.mobPhoto}
-            src={require('../../pictures/SearchPage/vegetables-5abfb9c60122f5 1.png')}
+            src={require('../../pictures/userDefault.png')}
             alt="vegetables"
           />
           <img
             className={searchCss.tabPhoto}
-            src={require('../../pictures/SearchPage/vegetables-5abfb9c60122f5 1_tab.png')}
+            src={require('../../pictures/userDefault.png')}
             alt="vegetables"
           />
         </div>
@@ -72,7 +68,7 @@ const SearchPage = () => {
       {recipes.length && (
         <>
           <div className={`${css.container} ${searchCss.container}`}>
-            <SearchedRecipesList recipes={ recipes} />
+            <SearchedRecipesList recipes={recipes} />
           </div>
           <div className={`${searchCss.paginationWrap} `}>
             <Pagination count={5} page={page} onChange={handleChange} />
