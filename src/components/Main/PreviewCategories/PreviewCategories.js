c3c4e4596b3f7a0726afec0a5fb03c7a312c9c56
PokoenkoS <pokoenkosveta@gmail.com>
2023-07-16 23:07:37 +0300

    add styles

--- src/components/Main/PreviewCategories/PreviewCategories.js
+++ src/components/Main/PreviewCategories/PreviewCategories.js
@@ -8,22 +8,13 @@ export const PreviewCategories =()=>{
 
 const recipes = useSelector(selectRecipes);
 
-// recipes[1] = recipes.splice(0,1, recipes[1])[0]
-// console.log(recipes);
-// for (const value of recipes ) {
-//     console.log(value[1]);
-// }
-// const x = recipes.map(res=> [...res].sort((a,b)=>b-a))
-// console.log(x);
-// const newRecipes = [...x].sort((a,b)=>b-a)
-// console.log(newRecipes);
     return (
         
          <div className={css.container}>
-            <ul>
+            <ul  className={css.list}>
             {recipes.map(recipe=> {
                 return(
-                  <li key={recipe[0]._id}>
+                  <li className={css.list_item} key={recipe[0]._id}>
                 <section className={css.section}>
                <h2 className={css.title}>{recipe[0].category}</h2>  
                <RecipeList data={recipe}/>
@@ -33,47 +24,12 @@ const recipes = useSelector(selectRecipes);
                </section>
                 </li>
 
-     
                 ) 
            
             })}
-
-                
+     
             </ul>
            
-
-{/* 
-         <section className={css.section}>
-         <h2 className={css.title}>Breakfast</h2>
-         <RecipeList data={recipes.Breakfast}/>
-         <Link to="/categories/:breakfast">
-         <button className={css.btn} type="button">See all</button>
-         </Link>
-         
-         </section>
-  
- <section className={css.section}>
-     <h2 className={css.title}>Miscellaneous</h2>
-     <RecipeList data={recipes.Miscellaneous}/>
-     <Link to="/categories/:miscellaneous">
-     <button className={css.btn} type="button">See all</button>
-     </Link>
-     
-     </section>
- <section className={css.section}>
-     <h2 className={css.title}>Chicken</h2>
-     <RecipeList data={recipes.Chicken}/>
-     <Link to="/categories/:chicken">
-     <button className={css.btn} type="button">See all</button>
-     </Link>
-     </section>
- <section className={css.section}>
-     <h2 className={css.title}>Deserts</h2>
-     <RecipeList data={recipes.Deserts}/>
-     <Link to="/categories/:deserts">
-     <button className={css.btn} type="button">See all</button>
-     </Link>
-     </section> */}
  <Link className={css.btn_other} to="/categories">Other categories</Link>
  </div>
         
--- src/components/Main/RecipeList/RecipesList.js
+++ src/components/Main/RecipeList/RecipesList.js
@@ -13,9 +13,14 @@ export function RecipeList({data}) {
 return (
 
 <li key={recipe._id} className={css.list_item}>
-       <Link to={`/recipe/:${recipe._id}`}>
-       <h3 className={css.list_title}>{recipe.title}</h3>
+       <Link
+       to=
+        // {`/recipe?id=${recipe._id}`}
+          {`/recipe/${recipe._id}`} 
+        >
+      
        <img src={recipe.thumb} alt="recipe" className={css.img}/>
+       <h3 className={css.list_title}>{recipe.title}</h3>
        </Link>
       
   </li>
