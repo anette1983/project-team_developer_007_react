e01a00ebbdbcd83c021d688bfedf05475f491331
Hanna <anna_kon@bigmir.net>
2023-07-18 23:30:35 +0300

    Added notification on the Main page search

--- src/components/Main/Search/Search.js
+++ src/components/Main/Search/Search.js
@@ -1,30 +1,36 @@
-import {  useState } from "react"
-import { Link } from "react-router-dom";
-import css from "./Search.module.css";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import css from './Search.module.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export const Search =()=> {
-const [value, setValue] = useState ('');
+export const Search = () => {
+  const [value, setValue] = useState('');
 
-const handleChange = e => setValue(e.currentTarget.value.toLowerCase());
+  const handleChange = e => setValue(e.currentTarget.value.toLowerCase());
 
-    return (
-        <form className={css.form}>
-            <input
-            className={css.input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            value={value}
-            onChange={handleChange}
-            placeholder="Enter the text"
-            />
-            <Link
-           to={value ? `/search/?query=${value}` : '#'}
-            
-            >
-                
-            <button className={css.btn} type="submit">Search</button>
-            </Link>
-            </form>
-    )
-}
+  const handleLinkClick = e => {
+    if (!value) {
+      e.preventDefault();
+      return Notify.failure('Enter ingredient!');
+    }
+  };
+
+  return (
+    <form className={css.form}>
+      <input
+        className={css.input}
+        type="text"
+        autoComplete="off"
+        autoFocus
+        value={value}
+        onChange={handleChange}
+        placeholder="Enter the text"
+      />
+      <Link to={`/search/?query=${value}`} onClick={handleLinkClick}>
+        <button className={css.btn} type="submit">
+          Search
+        </button>
+      </Link>
+    </form>
+  );
+};
