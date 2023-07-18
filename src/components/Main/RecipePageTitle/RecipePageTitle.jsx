35a39fbb6dd356bdd1439aa96f82b9ffcef2e1fb
Maksym-Babich <maxus1042@gmail.com>
2023-07-18 08:37:50 +0300

    +

--- src/components/Main/RecipePageTitle/RecipePageTitle.jsx
+++ src/components/Main/RecipePageTitle/RecipePageTitle.jsx
@@ -1,10 +1,11 @@
 import { useSelector } from 'react-redux';
 import { selectCurrentRecipe } from 'redux/recipes/selectors';
+import { ReactComponent as Clock } from '../../../images/footer/clock.svg';
 
 import css from './RecipePageTitle.module.css';
 
 export const RecipePageTitle = () => {
-  const { hero, title: CssTitle, subtitle, button, time_prepeare } = css;
+  const { hero, title: CssTitle, subtitle, button, time_prepeare, icon } = css;
   const recipe = useSelector(selectCurrentRecipe);
 
   return (
@@ -14,7 +15,10 @@ export const RecipePageTitle = () => {
       <button type="button" className={button}>
         Add to favorite recipes
       </button>
-      <p className={time_prepeare}>{recipe.time}</p>
+      <p className={time_prepeare}>
+        <Clock className={icon} />
+        <span>{recipe.time} min</span>
+      </p>
     </section>
   );
 };
