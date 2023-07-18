5472318357d1bf6608aac25d6191b8599bb81a16
MariaPetrukhnova <110037119+MariaPetrukhnova@users.noreply.github.com>
2023-07-18 23:23:43 +0300

    add popular recipes fetch and relink from it

--- src/components/AddRecipe/PopularRecipe.jsx
+++ src/components/AddRecipe/PopularRecipe.jsx
@@ -2,16 +2,20 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectPopularRecipes } from "../../redux/popularRecipes/selectors";
 import { getPopularRecipes } from "../../redux/popularRecipes/operations";
+import { Link } from "react-router-dom";
+
 import css from './AddRecipeComponents.module.css';
 import recipeMock from "../../images/addrecipe/add_photo_icon_x1.png";
 
 export default function PopularRecipe() {
   const dispatch = useDispatch();
-  const popularRecipes = useSelector(selectPopularRecipes);
+  const popularRecipesObj = useSelector(selectPopularRecipes);
+  const popularRecipes = popularRecipesObj?.recipes || [];
 
   useEffect(() => {
     dispatch(getPopularRecipes());
   }, [dispatch]);
+
 
   return (
     <div>
@@ -19,13 +23,13 @@ export default function PopularRecipe() {
       {popularRecipes.length > 0 ? (
         <ul className={css.popularRecipesList}>
           {popularRecipes.map(recipe => <li className={css.popularRecipesItem} key={recipe._id}>
-            <a href={`/recipe/${recipe._id}`} className={css.popularRecipesLink}>
+            <Link to={`/recipe/${recipe._id}`} className={css.popularRecipesLink}>
               <img src={recipe.preview || recipeMock} alt="popularDish" className={css.recipePreview} />
               <div>
                 <h5 className={css.popularRecipeSubtitle}>{recipe.title}</h5>
                 <p className={css.popularRecipeText}>{recipe.instructions}</p>
               </div>
-            </a>
+            </Link>
           </li>)}
         </ul>
       ) : (
