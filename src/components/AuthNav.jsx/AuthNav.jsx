0f07792a2eaaa18be793eaecc5c270bd7f6b1631
ViktorSavchenko <e_d_d.opk@ukr.net>
2023-07-19 12:42:58 +0300

    bf start and reg/sign form
    1. Change Reg battom
    2. Add h/f for links

--- src/components/AuthNav.jsx/AuthNav.jsx
+++ src/components/AuthNav.jsx/AuthNav.jsx
@@ -5,10 +5,10 @@ const AuthNav = () => {
   return (
     <>
       <div className={css.AuthNav__wrap}>
-        <Link className={css.AuthNav__button} to="/register">
+        <Link className={css.AuthNav__button_reg} to="/register">
           Registration
         </Link>
-        <Link className={css.AuthNav__button} to="/signin">
+        <Link className={css.AuthNav__button_sign} to="/signin">
           Sign In
         </Link>
       </div>
