dce2e82c5f4099dc62e6dc2de78cffdad16dcb09
SergSmilya <sergsmilichenko@gmail.com>
2023-07-19 19:07:11 +0300

    Update FavoritePage.jsx

--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,13 +1,11 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-// import { MainPageTitle } from 'components/MainPageTitle/MainPageTitle';
 import css from './FavoritePage.module.css';
 import FavoriteList from 'components/FavoriteList/FavoriteList';
 import { selectFavoriteRecipes } from 'redux/favoriteRecipes/selectors';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchFavoriteRecipes } from 'redux/favoriteRecipes/operations';
 import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
-// import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 
 const FavoritePage = () => {
   const { pathname } = useLocation();
@@ -24,20 +22,58 @@ const FavoritePage = () => {
 
   return (
     <div className={css.wrapper}>
-      {favoriteRecipes?.length === 0 ? (
-        <h1>No favorite recipes</h1>
-      ) : (
-        <>
-          <section className={css.favSection}>
-            {favoriteRecipes && (
-              <NotFoundPage text="you have no personal recipes" />
-            )}
-            <FavoriteList favoriteRecipes={favoriteRecipes}></FavoriteList>
-          </section>
-        </>
-      )}
+      <section className={css.favSection}>
+        {favoriteRecipes.length !== 0 ? (
+          <FavoriteList favoriteRecipes={favoriteRecipes}></FavoriteList>
+        ) : (
+          <NotFoundPage text="you have no favorite recipes" />
+        )}
+      </section>
     </div>
   );
 };
 
 export default FavoritePage;
+
+// import { useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
+// // import { MainPageTitle } from 'components/MainPageTitle/MainPageTitle';
+// import css from './FavoritePage.module.css';
+// import FavoriteList from 'components/FavoriteList/FavoriteList';
+// import { selectFavoriteRecipes } from 'redux/favoriteRecipes/selectors';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchFavoriteRecipes } from 'redux/favoriteRecipes/operations';
+// import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+
+// const FavoritePage = () => {
+//   const { pathname } = useLocation();
+//   const dispatch = useDispatch();
+//   const favoriteRecipes = useSelector(selectFavoriteRecipes);
+
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
+
+//   useEffect(() => {
+//     dispatch(fetchFavoriteRecipes());
+//   }, [dispatch]);
+
+//   return (
+//     <div className={css.wrapper}>
+//       {favoriteRecipes?.length === 0 ? (
+//         <h1>No favorite recipes</h1>
+//       ) : (
+//         <>
+//           <section className={css.favSection}>
+//             {favoriteRecipes && (
+//               <NotFoundPage text="you have no personal recipes" />
+//             )}
+//             <FavoriteList favoriteRecipes={favoriteRecipes}></FavoriteList>
+//           </section>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default FavoritePage;
