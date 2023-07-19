a59b21256cab92d1ce5b620d48b71b9603599729
Pavel S <neutr00n90@gmail.com>
2023-07-19 07:49:23 +0300

    bugfix register navigate

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,6 @@
 import { SignUpForm } from 'components/SignUpForm/SignUpForm';
 import { useSelector } from 'react-redux';
 import { selectAuthError } from 'redux/auth/selectors';
-import { Navigate } from 'react-router-dom';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const RegisterPage = () => {
@@ -11,7 +10,6 @@ const RegisterPage = () => {
     Notify.failure('Sorry, something went wrong');
   } else {
     Notify.success('Verification letter was send to your email');
-    return <Navigate to="/signin" />;
   }
 
   return (
