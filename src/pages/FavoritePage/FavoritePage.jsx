33b37f339ba6f1bcb1b4f0299ea876dd18be6a2a
Oleksii Kostenko <kostenko.oleksii91@gmail.com>
2023-07-21 19:55:06 +0300

    done

--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,51 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import css from './FavoritePage.module.css';
 import FavoriteList from 'components/FavoriteList/FavoriteList';
 import { selectFavoriteRecipes } from 'redux/favoriteRecipes/selectors';
 import { useDispatch, useSelector } from 'react-redux';
 
-import {
-  deleteFavoriteRecipi,
-  fetchFavoriteRecipes,
-} from 'redux/favoriteRecipes/operations';
+import { fetchFavoriteRecipes } from 'redux/favoriteRecipes/operations';
 
 import searchCss from '../SearchPage/searchContainer.module.css';
 import { SectionTitle } from 'components/SectionTitle/SectionTitle';
-
 
 const FavoritePage = () => {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
   const favoriteRecipes = useSelector(selectFavoriteRecipes);
-  const [data, setData] = useState('');
 
-  const childToParent = data => {
-    setData(data);
-  };
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
   useEffect(() => {
     dispatch(fetchFavoriteRecipes());
-    if (data !== '') {
-      dispatch(deleteFavoriteRecipi(data));
-      dispatch(fetchFavoriteRecipes());
-    }
-  }, [dispatch, data]);
+  }, [dispatch]);
 
   return (
     <div className={css.wrapper}>
-
       <SectionTitle text={'Favorites'} />
 
       <section className={css.favSection}>
         {favoriteRecipes.length !== 0 ? (
-          <FavoriteList
-            childToParent={childToParent}
-            favoriteRecipes={favoriteRecipes}
-          ></FavoriteList>
+          <FavoriteList favoriteRecipes={favoriteRecipes}></FavoriteList>
         ) : (
           <div className={css.img_wrapp}>
             <img
