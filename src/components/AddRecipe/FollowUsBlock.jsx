1baf2a02b3350d39dbb1e6d1419a1038880220a7
ViktorSavchenko <e_d_d.opk@ukr.net>
2023-07-21 12:35:25 +0300

    change css

--- src/components/AddRecipe/FollowUsBlock.jsx
+++ src/components/AddRecipe/FollowUsBlock.jsx
@@ -1,4 +1,5 @@
-import { FollowUs } from '../Footer/FollowUs/FollowUs';
+// import { FollowUs } from '../Footer/FollowUs/FollowUs';
+import sprite from '../../images/svg/sprite.svg';
 
 import css from './AddRecipeComponents.module.css';
 
@@ -7,7 +8,36 @@ export const FollowUsBlock = () => {
     <div className={css.socialsWrapper}>
       <h3 className={css.subtitle}> Follow us </h3>
       <div className={css.followUsBlock}>
-      <FollowUs />
+      <ul className={css.list}>
+        <li className={css.item}>
+          <a href=" ">
+            <svg width="26" height="26" className={css.soc_icon}>
+              <use href={sprite + '#face'} />
+            </svg>
+          </a>
+        </li>
+        <li className={css.item}>
+          <a href=" ">
+            <svg width="28" height="22" className={css.soc_icon}>
+              <use href={sprite + '#youtube'} />
+            </svg>
+          </a>
+        </li>
+        <li className={css.item}>
+          <a href=" ">
+            <svg width="25" height="22" className={css.soc_icon}>
+              <use href={sprite + '#twitter'} />
+            </svg>
+          </a>
+        </li>
+        <li className={css.item}>
+          <a href=" ">
+            <svg width="24" height="24" className={css.soc_icon}>
+              <use href={sprite + '#insta'} />
+            </svg>
+          </a>
+        </li>
+      </ul>
       </div>
     </div>
   )
