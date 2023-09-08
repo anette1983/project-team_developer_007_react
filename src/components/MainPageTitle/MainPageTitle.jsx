0b66ebed301238b35dd974fef21e5160358979ec
Hanna <anna_kon@bigmir.net>
2023-09-08 22:42:25 +0300

    fixed main title, header, main page css, work in progress

--- src/components/MainPageTitle/MainPageTitle.jsx
+++ src/components/MainPageTitle/MainPageTitle.jsx
@@ -1,20 +1,59 @@
-import css from './mainPage.module.css'
+import css from './mainPage.module.css';
 export const MainPageTitle = ({ text }) => {
-    return (
-        <>
-            <h1 className={css.title}>{text}</h1>
-                <svg className={css.firstGreen} width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect y="2.84525" width="8" height="8" rx="3" transform="rotate(-25 0 2.84525)" fill="#8BAA36" />
-                </svg>
-                <svg className={css.black}  width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect y="2" width="6" height="6" rx="3" transform="rotate(-25 0 2)" fill="#22252A" />
-                </svg>
-                <svg className={css.lastGreen}  width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect y="2.84525" width="8" height="8" rx="3" transform="rotate(-25 0 2.84525)" fill="#8BAA36" />
-                </svg>
-          
-            
-        </>
-    )
-    
-}
+  return (
+    <>
+      <h1 className={css.title}>{text}</h1>
+      <svg
+        className={css.firstGreen}
+        viewBox="0 0 18 18"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          y="5.91666"
+          width="14"
+          height="14"
+          rx="3"
+          transform="rotate(-25 0 5.91666)"
+          fill="#8BAA36"
+        />
+      </svg>
+      <svg
+        className={css.black}
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          y="5.07141"
+          width="12"
+          height="12"
+          rx="3"
+          transform="rotate(-25 0 5.07141)"
+          fill="#22252A"
+        />
+      </svg>
+      <svg
+        className={css.lastGreen}
+        viewBox="0 0 18 18"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          y="5.91666"
+          width="14"
+          height="14"
+          rx="3"
+          transform="rotate(-25 0 5.91666)"
+          fill="#8BAA36"
+        />
+      </svg>
+    </>
+  );
+};
+
+
+
+
+
+
