e194207745751ba431362abc6c7d33d48e1c1b74
Hanna <anna_kon@bigmir.net>
2023-09-09 18:43:36 +0300

    fixed css for tab and mobile breakpoints

--- src/components/Footer/Navigation/Navigation.js
+++ src/components/Footer/Navigation/Navigation.js
@@ -1,32 +1,38 @@
-import { FollowUs } from "../FollowUs/FollowUs"
-import { Nav } from "../Nav/Nav"
-import { SubscribeForm } from "../SubscribeForm/SubscribeForm"
-import css from "./Navigation.module.css";
-import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../logo/logo.svg"
+import { FollowUs } from '../FollowUs/FollowUs';
+import { Nav } from '../Nav/Nav';
+import { SubscribeForm } from '../SubscribeForm/SubscribeForm';
+import css from './Navigation.module.css';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../logo/logo.svg';
 
-export const Navigation =()=>{
-return (
-  <div className={css.container}>
-    <div>
-     <div className={css.logo}>
-      <Link to="/main" > <Logo/>
-             </Link>
-      <h3 className={css.title}>So Yummy</h3>
+export const Navigation = () => {
+  return (
+    <div className={css.container}>
+      <div>
+        <div className={css.logo}>
+          <Link to="/main">
+            <Logo />
+          </Link>
+          <h3 className={css.title}>So Yummy</h3>
+        </div>
+        <div className={css.wrapper}>
+          <ul className={css.list}>
+            <li className={css.item}>
+              Database of recipes that can be replenished{' '}
+            </li>
+            <li className={css.item}>
+              Flexible search for desired and unwanted ingredients
+            </li>
+            <li className={css.item}>
+              Ability to add your own recipes with photos
+            </li>
+            <li className={css.item}>Convenient and easy to use</li>
+          </ul>
+        </div>
       </div>
-       <div className={css.wrapper}>
-         <ul className={css.list}>
-          <li className={css.item}>Database of recipes that can be replenished </li>
-          <li className={css.item}>Flexible search for desired and unwanted ingredients</li>
-          <li className={css.item}>Ability to add your own recipes with photos</li>
-          <li className={css.item}>Convenient and easy to use</li>
-        </ul>
-       </div>
-       </div>
-  <Nav/>
-  <SubscribeForm/>
-  <FollowUs/>
- </div>
-   
-)
-}
+      <Nav />
+      <SubscribeForm />
+      <FollowUs />
+    </div>
+  );
+};
